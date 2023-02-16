c25d58a2b35955b0f581d353527028596bc02205
JakeBalakrishna <jake.balakrishna@aaoglobal.com>
2023-02-16 09:46:34 +0000

    App-finished

--- udemy-course-8/01-starting-project/src/components/Users/AddUser.js
+++ udemy-course-8/01-starting-project/src/components/Users/AddUser.js
@@ -1,23 +1,78 @@
-import react from 'react';
-import Card from '../UI/Card';
-import classes from './AddUser.module.css';
-import Button from '../UI/Button';
+import react, { useState } from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
-const AddUser = props => {
-    const addUserHandler = (event) => {
-        event.preventDefault();
-    };
-   return ( 
-    <Card className={classes.input}>
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredUsername, enteredAge);
+    if (enteredUsername.trim().length === 0 || enteredAge.trim() === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Oh FUUk!! plz enter a valid name and age",
+      });
+      return;
+    }
+    if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Oh FUUk!! plz enter a valid age (> 0)",
+      });
+      return;
+    }
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  const usernameCangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageCangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
-            <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number" />
-            <Button type="submit">Add User</Button>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameCangeHandler}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageCangeHandler}
+          />
+          <Button type="submit">Add User</Button>
         </form>
-   </Card>
-   );
+      </Card>
+    </div>
+  );
 };
 
-export default AddUser; 
+export default AddUser;
